7fbb4b78bf53ba66f5f75367bbf5a32e566a889c
rajesh <rajmadras75@gmail.com>
2025-05-10 15:33:19 -0700

    Update demo.js
    first demo code

--- demo.js
+++ demo.js
@@ -1,33 +1,34 @@
-<script src="matter.js"></script>
-// module aliases
-var Engine = Matter.Engine,
-    Render = Matter.Render,
-    Runner = Matter.Runner,
-    Bodies = Matter.Bodies,
-    Composite = Matter.Composite;
+// Module aliases
+const Engine = Matter.Engine,
+      Render = Matter.Render,
+      Runner = Matter.Runner,
+      Bodies = Matter.Bodies,
+      Composite = Matter.Composite;
 
-// create an engine
-var engine = Engine.create();
+// Create an engine
+const engine = Engine.create();
+const world = engine.world;
 
-// create a renderer
-var render = Render.create({
-    element: document.body,
-    engine: engine
+// Create a renderer
+const render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    width: 800,
+    height: 600,
+    wireframes: false
+  }
 });
 
-// create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-
-// add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB, ground]);
-
-// run the renderer
 Render.run(render);
 
-// create runner
-var runner = Runner.create();
-
-// run the engine
+// Create runner
+const runner = Runner.create();
 Runner.run(runner, engine);
+
+// Add bodies
+const boxA = Bodies.rectangle(400, 200, 80, 80);
+const boxB = Bodies.rectangle(450, 50, 80, 80);
+const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+Composite.add(world, [boxA, boxB, ground]);
